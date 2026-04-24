2b2f5ad67c15eb95fd1fdcdbbb4d02b2fc7a07be
TSTC123 <2735056035@qq.com>
2026-04-24 10:31:55 +0800

    Fixed the linking error related to __real_dlopen.

--- dlopen_wrapper.c
+++ dlopen_wrapper.c
@@ -2,49 +2,50 @@
 #include <stdio.h>
 #include <string.h>
 
-// Declare the real dlopen function that the linker will provide
-extern void* __real_dlopen(const char* filename, int flag);
+// Use dlsym to get the real dlopen at runtime instead of relying on
+// linker --wrap mechanism, which is sensitive to link order.
+typedef void* (*real_dlopen_t)(const char*, int);
 
-// Keep track of recursive calls to prevent infinite loops
+static real_dlopen_t get_real_dlopen(void) {
+    static real_dlopen_t real_fn = NULL;
+    if (!real_fn) {
+        real_fn = (real_dlopen_t)dlsym(RTLD_NEXT, "dlopen");
+    }
+    return real_fn;
+}
+
 static int in_dlopen_wrapper = 0;
 
-// Wrapper function for dlopen to satisfy Qt6's symbol requirements
 void* __wrap_dlopen(const char* filename, int flag) {
-    // Prevent infinite recursion
     if (in_dlopen_wrapper) {
         return NULL;
     }
-    
+
     in_dlopen_wrapper = 1;
     void* result = NULL;
-    
-    // For a static build, we generally don't want to load dynamic libraries
-    // However, we allow VAAPI libraries for hardware acceleration
-    
+    real_dlopen_t real_dlopen = get_real_dlopen();
+
     if (filename) {
-        // Allow VAAPI libraries for hardware acceleration
-        if (strstr(filename, "libva") || 
-            strstr(filename, "va.so") || 
-            strstr(filename, "va-drm") || 
+        if (strstr(filename, "libva") ||
+            strstr(filename, "va.so") ||
+            strstr(filename, "va-drm") ||
             strstr(filename, "va-x11") ||
             strstr(filename, "vaapi")) {
             printf("Static build: Allowing VAAPI library: dlopen(\"%s\")\n", filename);
-            result = __real_dlopen(filename, flag);
+            result = real_dlopen ? real_dlopen(filename, flag) : dlopen(filename, flag);
         }
-        // Allow other essential graphics/video libraries
         else if (strstr(filename, "libdrm") ||
                  strstr(filename, "libEGL") ||
                  strstr(filename, "libGL")) {
             printf("Static build: Allowing graphics library: dlopen(\"%s\")\n", filename);
-            result = __real_dlopen(filename, flag);
+            result = real_dlopen ? real_dlopen(filename, flag) : dlopen(filename, flag);
         }
         else {
-            // Block all other dynamic libraries
             printf("Static build: dlopen(\"%s\") disabled\n", filename);
             result = NULL;
         }
     }
-    
+
     in_dlopen_wrapper = 0;
     return result;
 }
